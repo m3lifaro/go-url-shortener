c7a96480643fe0f8200d4ab3ae584cf3b95b183d
melifaro <melifaro.nbk@gmail.com>
2025-07-14 20:07:16 +0300

    http-client now runs on resty

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,50 +3,29 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io"
-	"net/http"
+	"github.com/go-resty/resty/v2"
 	"os"
 	"strings"
 )
 
 func main() {
 	endpoint := "http://localhost:8080/"
-	// контейнер данных для запроса
-	// приглашение в консоли
 	fmt.Println("Введите длинный URL")
-	// открываем потоковое чтение из консоли
 	reader := bufio.NewReader(os.Stdin)
-	// читаем строку из консоли
 	long, err := reader.ReadString('\n')
 	if err != nil {
 		panic(err)
 	}
 	long = strings.TrimSuffix(long, "\n")
-	// заполняем контейнер данными
-	// добавляем HTTP-клиент
-	client := &http.Client{}
-	// пишем запрос
-	// запрос методом POST должен, помимо заголовков, содержать тело
-	// тело должно быть источником потокового чтения io.Reader
-	request, err := http.NewRequest(http.MethodPost, endpoint, strings.NewReader(long))
+	client := resty.New()
+
+	response, err := client.R().
+		SetBody(strings.NewReader(long)).
+		SetHeader("Content-Type", "text/plain").
+		Post(endpoint)
 	if err != nil {
 		panic(err)
 	}
-	// в заголовках запроса указываем кодировку
-	request.Header.Add("Content-Type", "text/plain")
-	// отправляем запрос и получаем ответ
-	response, err := client.Do(request)
-	if err != nil {
-		panic(err)
-	}
-	// выводим код ответа
-	fmt.Println("Статус-код ", response.Status)
-	defer response.Body.Close()
-	// читаем поток из тела ответа
-	body, err := io.ReadAll(response.Body)
-	if err != nil {
-		panic(err)
-	}
-	// и печатаем его
-	fmt.Println(string(body))
+	fmt.Println("Статус-код ", response.Status())
+	fmt.Println("Тело ответа ", response)
 }
