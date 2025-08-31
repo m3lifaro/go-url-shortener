b13ce8a82b8afd404beb55877de7e4a091be1f0b
melifaro <melifaro.nbk@gmail.com>
2025-09-01 00:50:35 +0300

    fix dereference log

--- internal/handler/middleware.go
+++ internal/handler/middleware.go
@@ -135,7 +135,6 @@ func authMiddlewareOptional(logger *zap.Logger, auz *auth.Auth) func(http.Handle
 			var hasAuth bool
 
 			cookie, err := r.Cookie(auth.CookieName)
-			println(cookie.Value)
 			if err == nil {
 				claims, err := auz.ParseJWT(cookie.Value)
 				if err == nil {
